e84b8d8b41c9baef2361792cef75c2ffc910ffc3
Simon <f.simon.qin@gmail.com>
2026-05-27 20:06:43 +0200

    chore: add minimal Vite entry points for examples

---
+++ examples/racing-demo/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1>Telemetriq Racing Demo</h1>
+    <p>Coming soon...</p>
+  </React.StrictMode>,
+);
